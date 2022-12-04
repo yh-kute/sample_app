9ba83a5ed73709ccb954b08d3c97515c9288b469
yh-kute <j120224@ns.kogakuin.ac.jp>
2022-12-04 14:00:46 +0000

    Finish layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,13 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "hello, world!"
   end
-  
+   # ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
